e3305e15ffeb5904d09a002757ccfa563d4d284e
Andres Meza <david0920md@gmail.com>
2024-11-20 09:19:59 -0500

    feature:se añadieron validaciones

--- src/app/components/incrementador/incrementador.component.ts
+++ src/app/components/incrementador/incrementador.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-incrementador',
@@ -6,9 +6,20 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class IncrementadorComponent {
-  progreso: number = 60;
+export class IncrementadorComponent  implements OnInit {
+  
+  
+  ngOnInit() {
+    this.btnClass = `btn ${this.btnClass}`; 
+  }
 
+
+  @Input ('valor') progreso: number = 60;
+  @Input () btnClass: string = 'btn-primary';
+
+
+  @Output ('valor') valorSalida: EventEmitter <number> = new EventEmitter();
+  
   get getPorcentaje () {
     return `${ this.progreso }% `;
   }
@@ -16,18 +27,32 @@ export class IncrementadorComponent {
   cambiarValor(valor: number): number {
     if (this.progreso >= 100 && valor >= 0) {
       this.progreso = 100;
+      this.valorSalida.emit(100);
       return this.progreso; 
     }
   
     if (this.progreso <= 0 && valor < 0) {
       this.progreso = 0;
+      this.valorSalida.emit(0);
       return this.progreso;  
     }
   
     
     this.progreso = this.progreso + valor;
+    this.valorSalida.emit(this.progreso);
     return this.progreso;  
   }
 
+  onChange( nuevoValor: number ) {
+    if ( nuevoValor >= 100){
+      this.progreso = 100;
+    } else if ( nuevoValor <= 0) {
+      this.progreso = 0;
+    }else {
+      this.progreso = nuevoValor;
+    }
+    this.valorSalida.emit(this.progreso);
+  }
 
 }
+// data => {}------------- (data:type) => {}
--- src/app/progress/progress.component.ts
+++ src/app/progress/progress.component.ts
@@ -6,7 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./progress.component.css']
 })
 export class ProgressComponent {
+progreso1: number = 25;
+progreso2: number = 35;
 
-
-  
+get getprogreso1 () {
+  return `${this.progreso1}%`;
+}
+ 
+get getprogreso2 () {
+  return `${this.progreso2}%`;
+}
+// cambioValorHijo (valor:number) {
+//   console.log('hey',valor);
+// }
 }
